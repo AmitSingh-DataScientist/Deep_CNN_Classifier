6612d96ded53114573a97bfab05974d10fcee4ad
Amit Kumar Singh <amitsingh.dataanalytics.com>
2022-09-22 21:55:11 +0530

    some update

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,10 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version"
 # conda create --prefix venv python=3.8 -y
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./env python=3.8 -y # update version
 echo [$(date)]: "activating the environment"
 source activate ./env
+# source activate venvclear
 echo [$(date)]: "installing the dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"

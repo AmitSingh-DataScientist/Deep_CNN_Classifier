377181547b32540b2c31876f9979dbb46ece952e
Amit Kumar Singh <amitsingh.dataanalytics@gmail.com>
2022-09-27 12:48:04 +0530

    lint update

--- src/deepCNNClassifier/components/data_ingestion.py
+++ src/deepCNNClassifier/components/data_ingestion.py
@@ -1,6 +1,4 @@
 ## Data Component
-from cmath import log
-import imp
 import os
 import urllib.request as request
 from zipfile import ZipFile
@@ -9,6 +7,7 @@ from deepCNNClassifier import logger
 from deepCNNClassifier.utils import get_size
 from tqdm import tqdm
 from pathlib import Path
+
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
@@ -19,27 +18,34 @@ class DataIngestion:
         if not os.path.exists(self.config.local_data_file):
             logger.info(f"Download started...")
             filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+                url=self.config.source_URL, filename=self.config.local_data_file
             )
             logger.info(f"{filename} downloaded! with following info: \n{headers} ")
-        else:    
-            logger.info(f"File already exits of size: {get_size(Path(self.config.local_data_file))}")
-            
-    ## hidden method
+        else:
+            logger.info(
+                f"File already exits of size: {get_size(Path(self.config.local_data_file))}"
+            )
+
+    # hidden method
     def _get_updated_list_of_files(self, list_of_files):
-        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
-    
-    ## hidden method
+        return [
+            f
+            for f in list_of_files
+            if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)
+        ]
+
+    # hidden method
     def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
         target_filepath = os.path.join(working_dir, f)
         if not os.path.exists(target_filepath):
             zf.extract(f, working_dir)
-        
+
         if os.path.getsize(target_filepath) == 0:
-            logger.info(f"Removing file: {target_filepath} of size: {get_size(Path(target_filepath))}")
+            logger.info(
+                f"Removing file: {target_filepath} of size: {get_size(Path(target_filepath))}"
+            )
             os.remove(target_filepath)
-             
+
     def unzip_and_clean(self):
         logger.info(f"Unzipping file and removing unwanted file")
         with ZipFile(file=self.config.local_data_file, mode="r") as zf:

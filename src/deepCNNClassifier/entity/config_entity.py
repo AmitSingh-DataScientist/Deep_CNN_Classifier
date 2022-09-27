f528cdcceae5bcfc00a6ca6a1b2dd107fa7ac568
Amit Kumar Singh <amitsingh.dataanalytics@gmail.com>
2022-09-27 12:49:53 +0530

    format documentation

--- src/deepCNNClassifier/entity/config_entity.py
+++ src/deepCNNClassifier/entity/config_entity.py
@@ -1,13 +1,14 @@
-## Data Ingestion Configuration Part
+# Data Ingestion Configuration Part
 
 # Alternative to 'namedtuple'
 from dataclasses import dataclass
 from pathlib import Path
 
-@dataclass(frozen=True)    # this decorator make sure that it works like 'namedtuple'
+
+@dataclass(frozen=True)  # this decorator make sure that it works like 'namedtuple'
 class DataIngestionConfig:
     # class variables
-    root_dir : Path
-    source_URL : str
-    local_data_file :Path
-    unzip_dir : Path
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path

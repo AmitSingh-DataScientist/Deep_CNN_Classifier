dd7c758e7f7d6829e59e2b440710ded40f815d5a
Amit Kumar Singh <amitsingh.dataanalytics@gmail.com>
2022-09-29 22:08:09 +0530

    stage 02 added

---
+++ src/deepCNNClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,26 @@
+from deepCNNClassifier.config import ConfigurationManager
+from deepCNNClassifier.components import PrepareBaseModel
+from deepCNNClassifier import logger
+
+STAGE_NAME = "Prepare base model"
+
+
+def main():
+    config = ConfigurationManager()
+    prepare_base_model_config = config.get_prepare_base_model_config()
+    prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+    prepare_base_model.get_base_model()
+    prepare_base_model.update_base_model()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"**********************************************")
+        logger.info(f">>>>>>> stage {STAGE_NAME} is started <<<<<<<<")
+        main()
+        logger.info(
+            f">>>>>>> stage {STAGE_NAME} is completed <<<<<<<<\n\nx============x"
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise e
